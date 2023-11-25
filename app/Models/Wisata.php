2ae2d85fce67e0015b2852efa61593f91db1db1a
Nafka200136 <G231200136@student.usm.ac.id>
2023-11-25 07:52:53 +0700

    Rekayasa Web API

---
+++ app/Models/Wisata.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Wisata extends Model
+{
+
+    protected $table = "wisata";
+    protected $primaryKey = "id_wisata";
+    public $timestamps =  false;
+
+    protected $fillable = [
+      "kota", "landmark", "tarif"
+    ];
+
+    protected $hidden = [];
+
+}
